5b03ad5a46d6cf3fcbdc0959e2b0c1917290ae1c
joshthomas98 <joshua.thomas98@hotmail.co.uk>
2023-10-25 02:38:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import Homepage from "./pages/Homepage";
+import SignIn from "./pages/SignIn";
+import OurServices from "./pages/OurServices";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div className="App">
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route index element={<Homepage />} />
+
+            <Route path="signin" element={<SignIn />} />
+
+            <Route path="ourservices" element={<OurServices />} />
+          </Routes>
+        </BrowserRouter>
+        {/* <Footer /> */}
+      </div>
+    </Container>
   );
 }
 
